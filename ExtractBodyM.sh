8e3fd462dd79e408bab9ee4c3ceed2164de059ad
Vanessa Ramos <ramosvsd@gmail.com>
2020-02-06 09:36:30 -0800

    latehw_sick

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv |cut -d ';' -f 2-6 | tr -s ';' ' ' | sort -r -n -k 6
+tail -n +2 $1 |cut -d $3 -f 2-6 | tr -s $3 ' ' | sort -r -n -k 6 > $2
 
 
